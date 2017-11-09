c0900ba118b178631e3b378b50e15089e849caca
Rich Gregorio <rich.gregorio@nuix.com>
2017-11-09 15:54:19 -0500

    initial commit

---
+++ lyrical.py
@@ -0,0 +1,42 @@
+import mechanize
+from bs4 import BeautifulSoup
+
+
+class Lyrical:
+    def __init__(self, url):
+        self.baseURL = url
+
+    def getLyrics(self):
+
+        br = mechanize.Browser()
+        r = br.open(self.baseURL)
+
+        soup = BeautifulSoup(r.read(), 'html.parser')
+        verses = soup.select("p.verse")
+
+        song = ""
+
+        for verse in verses:
+            song = song + " " + verse.get_text().lower()
+            
+
+        wordlist = song.split()
+
+        x = self.wordListToFreqDict(wordlist)
+        y = self.sortFreqDict(x)
+    
+        for z in y:
+            print z
+
+    def wordListToFreqDict(self, wordlist) : 
+        wordfreq = [wordlist.count(p) for p in wordlist]
+        return dict(zip(wordlist,wordfreq))
+
+    def sortFreqDict(self, freqdict):
+        aux = [(freqdict[key], key) for key in freqdict]
+        aux.sort()
+        aux.reverse()
+        return aux
+
+
+Lyrical("http://www.metrolyrics.com/walking-on-the-moon-lyrics-the-police.html").getLyrics()
